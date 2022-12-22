15ce79777d4f10bc0a29c2fa44cf51e57815e5c5
NJS <njongsung@gmail.com>
2022-12-22 11:50:50 +0900

    test ok

--- admin/src/main/java/com/shop/dto/User.java
+++ admin/src/main/java/com/shop/dto/User.java
@@ -1,5 +1,17 @@
 package com.shop.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
 public class User {
 	private String id;
+	private String pwd;
 }
