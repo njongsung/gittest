31a5a27de2d5e69ef3846723f81c224997f72a8a
NJS <njongsung@gmail.com>
2022-12-22 11:44:45 +0900

    modify

--- admin/src/main/java/com/shop/dto/User.java
+++ admin/src/main/java/com/shop/dto/User.java
@@ -1,18 +1,5 @@
 package com.shop.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
-
-@AllArgsConstructor
-@NoArgsConstructor
-@Getter
-@Setter
-@ToString
 public class User {
 	private String id;
-	private String pwd;
-	private String name;
 }
